eefee36a29a5749a7b15593af3b87a94480c2f16
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-17 18:21:21 +0530

    Update replacer.sh

--- replacer.sh
+++ replacer.sh
@@ -11,12 +11,10 @@ if [ -f "$file_name" ]; then
     echo $line
     key=$(echo "$line" | cut -d '=' -f 1)
     value=$(echo "$line" | cut -d '=' -f 2)
-    sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo passward
+    sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo password
   done < $file_name
 else
   echo "The file $file_name does not exist."
 fi
 
 argocd-vault-plugin generate /tmp/tmp.yaml
-# kubectl apply -f /tmp/tmp.yaml
-# sudo rm -f /tmp/tmp.yaml
