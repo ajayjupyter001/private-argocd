0e6755d58cfcea83f9ea9597c5171aa2da6447e5
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-17 14:49:21 +0530

    Add files via upload

---
+++ manifest/overlays/prod/replacer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+kustomize build . > /tmp/tmp.yaml
+
+file_name="values.properties"
+
+if [ -f "$file_name" ]; then
+  echo "The file $file_name exists."
+  while IFS="\n" read -r line
+  do
+    echo $line
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2)
+    sudo sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo passward
+  done < $file_name
+else
+  echo "The file $file_name does not exist."
+fi
+
+argocd-vault-plugin generate /tmp/tmp.yaml
+# kubectl apply -f /tmp/tmp.yaml
+# sudo rm -f /tmp/tmp.yaml
