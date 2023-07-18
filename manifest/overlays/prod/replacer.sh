a7b249eaf9dae86b4331a1265a601242aa34de98
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-18 11:33:16 +0530

    Update replacer.sh

--- manifest/overlays/prod/replacer.sh
+++ manifest/overlays/prod/replacer.sh
@@ -2,19 +2,19 @@
 
 kustomize build . > /tmp/tmp.yaml
 
-file_name="values.properties"
+# file_name="values.properties"
 
-if [ -f "$file_name" ]; then
-  echo "The file $file_name exists."
-  while IFS="\n" read -r line
-  do
-    echo $line
-    key=$(echo "$line" | cut -d '=' -f 1)
-    value=$(echo "$line" | cut -d '=' -f 2)
-    sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo passward
-  done < $file_name
-else
-  echo "The file $file_name does not exist."
-fi
+# if [ -f "$file_name" ]; then
+#   echo "The file $file_name exists."
+#   while IFS="\n" read -r line
+#   do
+#     echo $line
+#     key=$(echo "$line" | cut -d '=' -f 1)
+#     value=$(echo "$line" | cut -d '=' -f 2)
+#     sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo passward
+#   done < $file_name
+# else
+#   echo "The file $file_name does not exist."
+# fi
 
 argocd-vault-plugin generate /tmp/tmp.yaml
