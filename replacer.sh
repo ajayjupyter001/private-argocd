685f50ce08573cf04665f93eaf55ef5730cc9006
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-18 12:00:11 +0530

    Update replacer.sh

--- replacer.sh
+++ replacer.sh
@@ -2,20 +2,20 @@
 
 kustomize build . > /tmp/tmp.yaml
 
-# file_name="values.properties"
+file_name="values.properties"
 
-# if [ -f "$file_name" ]; then
-#   echo "The file $file_name exists."
-#   while IFS="\n" read -r line
-#   do
-#     echo $line
-#     key=$(echo "$line" | cut -d '=' -f 1)
-#     value=$(echo "$line" | cut -d '=' -f 2)
-#     sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo password
-#   done < $file_name
-# else
-#   echo "The file $file_name does not exist."
-#   exit 90
-# fi
+if [ -f "$file_name" ]; then
+  echo "The file $file_name exists."
+  while IFS="\n" read -r line
+  do
+    echo $line
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2)
+    sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo password
+  done < $file_name
+else
+  echo "The file $file_name does not exist."
+  exit 90
+fi
 
 argocd-vault-plugin generate /tmp/tmp.yaml
