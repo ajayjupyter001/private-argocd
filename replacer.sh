f7da25128d8126cacdc02ef647151b8509b309cf
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-19 16:50:23 +0530

    Update replacer.sh

--- replacer.sh
+++ replacer.sh
@@ -4,6 +4,6 @@ kustomize build . > /tmp/tmp.yaml
 
 file_name="values.properties"
 
-sed -i "s/containername/value_From_ajay/g" /tmp/tmp.yaml
+sed -i "s/containername/ashwin/g" /tmp/tmp.yaml
 
 argocd-vault-plugin generate /tmp/tmp.yaml
