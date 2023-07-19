e10043e3c2897b0d93d995885d3118777ebbbf3c
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-19 12:33:48 +0530

    Update replacer.sh

--- replacer.sh
+++ replacer.sh
@@ -8,7 +8,6 @@ if [ -f "$file_name" ]; then
   echo "The file $file_name exists."
   while IFS="\n" read -r line
   do
-    echo $line
     key=$(echo "$line" | cut -d '=' -f 1)
     value=$(echo "$line" | cut -d '=' -f 2)
     sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo password
