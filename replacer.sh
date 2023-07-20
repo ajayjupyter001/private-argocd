d9f3f304c5642ff7bb4a8fef3f4ce1cd13dda82e
AJAY A <102233600+ajayjupyter001@users.noreply.github.com>
2023-07-20 11:02:54 +0530

    Update replacer.sh

--- replacer.sh
+++ replacer.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-kustomize build . > /tmp/tmp.yaml
-
 file_name="values.properties"
 
-while IFS="\n" read -r line
+if [ -f "$file_name" ]; then
+  while IFS="\n" read -r line
   do
     key=$(echo "$line" | cut -d '=' -f 1)
     value=$(echo "$line" | cut -d '=' -f 2)
-    sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo password
-done < $file_name
-
-argocd-vault-plugin generate /tmp/tmp.yaml
+    sudo sed -i "s/$key/$value/g" /tmp/tmp.yaml  #asked sudo passward
+  done < $file_name
+else
+fi
